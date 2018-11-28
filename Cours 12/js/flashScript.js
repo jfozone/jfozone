af3a16ec43f30c7d872082e94abc8322f7dc1970
jfozone <44419360+jfozone@users.noreply.github.com>
2018-11-27 19:50:29 -0500

    ajout cours 12

---
+++ Cours 12/js/flashScript.js
@@ -0,0 +1,75 @@
+// Script cours 27 novembre 2018
+
+//Déplacer Gauche
+$( "#boutonGauche" ).click(function() {
+		var	nombre = Number(prompt("Entrez le nombre "));
+
+});
+
+//Déplacer Droite
+$( "#boutonDroite" ).click(function() {
+	
+});
+
+//Changer image" 
+//Changez l'image de Flash1 à Flash 2 et vice-versa (une fois).
+$( "#boutonChangerImage" ).click(function() {
+	
+});
+
+//Enlever l'image
+//Faire disparaître l'image (fadeOut)
+$( "#boutonEnleverImage" ).click(function() {
+	
+});
+
+//Afficher l'image" 
+//Faire apparaître l'image (fadeIn)
+$( "#boutonAjouterImage" ).click(function() {
+	
+});
+		
+//Ajouter attribut
+//Ajoutez un attribut alt à votre image avec la description 'photo de Flash
+$( "#boutonAjouterAttribut" ).click(function() {
+	
+});
+		
+//Ajouter Titre" 
+//Ajoutez un titre en H1 'FLASH JQUERY' dans le haut de cette page 
+//avant la photo (before)
+$( "#boutonAjouterTitre" ).click(function() {
+	
+});
+		
+//Dupliquer
+//Flash est tellement rapide, ajoutez une autre image à côté de Flash à tous les cliques sur ce bouton!<br />
+$( "#boutonDupliquer" ).click(function() {
+	
+});
+
+//Ralentir
+//Flash ralentit, enlevez une image!
+$( "#boutonRalentir" ).click(function() {
+	
+});
+
+//Teleport
+//Flash se téléporte sous ces boutons et retourne en haut lorsque l'on clique de 
+//nouveau
+$( "#boutonTeleport" ).click(function() {
+	
+});
+
+//VitesseSupreme
+//Flash doit apparaître et disparaître toutes les 500MS pendant 5 secondes.
+$( "#boutonVitesseSupreme" ).click(function() {
+	
+});
+
+//Nuke
+//Ce bouton fait afficher une image d'explosion dans l'écran et enlève tout le reste
+//(boutons images, etc).
+$( "#boutonNuke" ).click(function() {
+	
+});
