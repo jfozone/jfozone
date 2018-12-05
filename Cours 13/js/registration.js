cdb28fad512fdbef0492252b29c836964c43fa70
jfozone <44419360+jfozone@users.noreply.github.com>
2018-12-04 22:04:50 -0500

    fin du lab

--- Cours 13/js/registration.js
+++ Cours 13/js/registration.js
@@ -1,31 +1,91 @@
 // Validation usager
 $( "#userId" ).change(function() {
-    console.log($('#userId').val());
+
     if (($('#userId').val() === "SLAY") || ($('#userId').val() === "Test")){
-        $('#alerteUserIdOK').collapse();
+        $('#alerteUserIdOK').hide();
         $('#alerteUserIdBAD').show();
     }
-    else{
-        $('#alerteUserIdBAD').collapse();
-        $('#alerteUserIdOK').show();
-    }
-});
-// Validation mot de passe
-$( "#passwordId" ).change(function() {
-    console.log($('#passwordId').val());
-//    if ($('#passwordId').val() ){
-        $('#alertPassWeak').show();
-        $('#alertPassMid').show();
-        $('#alertPassStrong').show();
- /*   }
     else{
         $('#alerteUserIdBAD').hide();
         $('#alerteUserIdOK').show();
     }
-    */
 });
 
+// Validation mot de passe
+$( "#passwordId" ).change(function() {
 
+var password = $('#passwordId').val();
+var i=0, char='';
+var nbNum=0,nbUpCase=0;
+
+console.log($('#passwordId').val()+' '+password.length);
+//console.log($('#passwordId').val().match(/));
+    if ($('#passwordId').val().length < 6 ){
+        $('#alertPassMid').hide();
+        $('#alertPassStrong').hide();
+        $('#alertPassWeak').show();
+    }
+    else if ($('#passwordId').val().length >= 6 ){
+
+ /*
+
+Shany Carle - 04-12-2018 21:16: https://stackoverflow.com/questions/1027224/how-can-i-test-if-a-letter-in-a-string-is-uppercase-or-lowercase-using-javascrip
+Shany Carle - 04-12-2018 21:16: Oui un REGEX mais il y a plus simple
+Shany Carle - 04-12-2018 21:17: Tu boucles lettre par lettre et tu peux faire : if (character === character.toUpperCase())
+Jean Fortin - 04-12-2018 21:19: C'est le lien que tu as envoyé?
+Shany Carle - 04-12-2018 21:24: Oui ça explique comment faire
+Shany Carle - 04-12-2018 21:25: Mettons que tu as une string :
+Shany Carle - 04-12-2018 21:25: var mot = "shany";
+Shany Carle - 04-12-2018 21:25: mot[0] === 's'
+Shany Carle - 04-12-2018 21:26: Tu peux aussi faire if(mot[0] === mot[0].toUpperCase() --> Retourne vrai si la première lettre est une majuscule.
+Jean Fortin - 04-12-2018 21:32: oui mais ca fait la premieère lettre seulement
+Shany Carle - 04-12-2018 21:34: Tu dois boucler
+Shany Carle - 04-12-2018 21:34: for(var i=0;i<mot.length;i++)
+Shany Carle - 04-12-2018 21:35: if (mot[i] === mot[i].toUpperCase()
+ */
+        // Test password pour chiffres et lettres
+        while (i < password.length){
+            char = password.charAt(i);
+            if (!isNaN(char * 1)){
+                nbNum++;
+            }
+            else{
+                if (char == char.toUpperCase()) {
+                    nbUpCase++;
+                }
+            }
+            i++;
+        }
+        console.log('i: '+i+' Chiffres: '+nbNum+', Majuscules: '+nbUpCase);
+
+        if (nbNum > 0 && nbUpCase > 0 ){
+            $('#alertPassWeak').hide();
+            $('#alertPassMid').hide();
+            $('#alertPassStrong').show();
+        }
+        else {
+            $('#alertPassWeak').hide();
+            $('#alertPassStrong').hide();
+            $('#alertPassMid').show();
+        }
+    }
+});
+
+// Validation match mot de passe
+$( "#passwordConfirm" ).change(function() {
+
+//console.log($('#passwordId').val().match(/));
+    if ($('#passwordId').val() === $('#passwordConfirm').val()){
+        $('#alertPwNoMatch').hide();
+        $('#alertPwMatch').show();
+    }
+    else{
+        $('#alertPwMatch').hide();
+        $('#alertPwNoMatch').show();
+    } 
+
+});
+// Envoi des données
 $("#boutonEnvoyer").click(function() {
     //Un champ Input
     alert($('#userEmail').val());
